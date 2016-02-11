b2c717840978c0fe0589ed507ab5c42d67055c3b
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-02-12 00:57:20 +0200

    Add original N Queens

---
+++ apps/nqueens.cpp
@@ -0,0 +1,36 @@
+  //  Naxos Solver:  A Constraint Programming Library           //
+ //  Copyright © 2007-2016  Nikolaos Pothitos                  //
+//  See ../../license/LICENSE for the license of the library  //
+
+#include <naxos.h>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+using namespace naxos;
+
+int  main (int argc, char *argv[])
+{
+        try {
+                int  N = (argc > 1) ? atoi(argv[1]) : 8;
+                NsProblemManager  pm;
+                NsIntVarArray  Var, VarPlus, VarMinus;
+                for (int i=0;  i < N;  ++i) {
+                        Var.push_back( NsIntVar(pm, 0, N-1) );
+                        VarPlus.push_back(  Var[i] + i );
+                        VarMinus.push_back( Var[i] - i );
+                }
+                pm.add( NsAllDiff(Var) );
+                pm.add( NsAllDiff(VarPlus) );
+                pm.add( NsAllDiff(VarMinus) );
+                pm.addGoal( new NsgLabeling(Var) );
+                while ( pm.nextSolution() != false )
+                        cout << Var << "\n";
+        } catch (exception& exc) {
+                cerr << exc.what() << "\n";
+                return  1;
+        } catch (...) {
+                cerr << "Unknown exception" << "\n";
+                return  1;
+        }
+}
