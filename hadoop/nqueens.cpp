4dd78b3f35af36ee13f05c11bc63a7064c546aad
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-03-10 21:55:19 +0200

    This is the new (kernel) coding style

--- hadoop/nqueens.cpp
+++ hadoop/nqueens.cpp
@@ -1,4 +1,4 @@
-  //  Naxos Solver:  A Constraint Programming Library           //
+  //  Naxos Solver: A Constraint Programming Library            //
  //  Copyright © 2007-2015  Nikolaos Pothitos                  //
 //  See ../../license/LICENSE for the license of the library  //
 
@@ -11,38 +11,38 @@
 using namespace std;
 using namespace naxos;
 
-int  main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
         try {
-                int  N = (argc > 1) ? atoi(argv[1]) : 8;
-                NsProblemManager  pm;
-                NsIntVarArray  Var, VarPlus, VarMinus;
-                for (int i=0;  i < N;  ++i) {
-                        Var.push_back( NsIntVar(pm, 0, N-1) );
-                        VarPlus.push_back(  Var[i] + i );
-                        VarMinus.push_back( Var[i] - i );
+                int N = (argc > 1) ? atoi(argv[1]) : 8;
+                NsProblemManager pm;
+                NsIntVarArray Var, VarPlus, VarMinus;
+                for (int i = 0; i < N; ++i) {
+                        Var.push_back(NsIntVar(pm, 0, N-1));
+                        VarPlus.push_back( Var[i] + i);
+                        VarMinus.push_back(Var[i] - i);
                 }
-                pm.add( NsAllDiff(Var) );
-                pm.add( NsAllDiff(VarPlus) );
-                pm.add( NsAllDiff(VarMinus) );
+                pm.add(NsAllDiff(Var));
+                pm.add(NsAllDiff(VarPlus));
+                pm.add(NsAllDiff(VarMinus));
                 // MapExplore specific code follows //
                 srand(time(0));
-                double  maxSplitTime, splitTime, simulationRatio;
-                if ( !( argc == 5 &&
-                        istringstream(argv[2]) >> maxSplitTime &&
-                        istringstream(argv[3]) >> splitTime &&
-                        istringstream(argv[4]) >> simulationRatio ) ) {
+                double maxSplitTime, splitTime, simulationRatio;
+                if (!(argc == 5 &&
+                      istringstream(argv[2]) >> maxSplitTime &&
+                      istringstream(argv[3]) >> splitTime &&
+                      istringstream(argv[4]) >> simulationRatio)) {
                         cerr << "Usage: " << argv[0]
                              << " <N> <max_split_time> <split_time> <simulation_ratio>\n";
                         return  1;
                 }
-                while ( pm.readSplit() ) {
+                while (pm.readSplit()) {
                         pm.timeLimit(maxSplitTime);
-                        pm.addGoal( new NsgLabeling(Var) );
-                        while ( pm.nextSolution() != false ) {
-                                for (int i=0;  i < N;  ++i) {
+                        pm.addGoal(new NsgLabeling(Var));
+                        while (pm.nextSolution() != false) {
+                                for (int i = 0; i < N; ++i) {
                                         cout << Var[i].value();
-                                        if ( i < N-1 )
+                                        if (i < N - 1)
                                                 cout << " ";
                                 }
                                 cout << "\n";
@@ -51,9 +51,9 @@ int  main (int argc, char *argv[])
                 }
         } catch (exception& exc) {
                 cerr << exc.what() << "\n";
-                return  1;
+                return 1;
         } catch (...) {
                 cerr << "Unknown exception" << "\n";
-                return  1;
+                return 1;
         }
 }
