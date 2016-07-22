2c052d681a307342aab35277d7d8420ac81f6142
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-07-23 01:08:48 +0300

    Upgrade Hadoop version

--- hadoop/install/update.sh
+++ hadoop/install/update.sh
@@ -1,14 +1,14 @@
-scp master:nikos/hadoop-2.7.0.tar.gz .
-scp master:nikos/hadoop_install/hadoop.patch .
+scp master:nikos/hadoop-2.7.2.tar.gz .
+scp master:nikos/naxos-solver/hadoop/install/hadoop.patch .
 
 cd /usr/local/
-sudo \rm -r hadoop-2.6.0/
+sudo \rm -r hadoop-2.7.1/
 
-sudo tar xzf ~/hadoop-2.7.0.tar.gz 
-\rm ~/hadoop-2.7.0.tar.gz 
+sudo tar xzf ~/hadoop-2.7.2.tar.gz
+\rm ~/hadoop-2.7.2.tar.gz
 
-sudo chown -R user:user hadoop-2.7.0/
-sudo ln -snf hadoop-2.7.0 hd
+sudo chown -R user:user hadoop-2.7.2/
+sudo ln -snf hadoop-2.7.2 hd
 
 cd hd
 patch -p1 -i ~/hadoop.patch
