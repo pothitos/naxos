31818295d2aab38b6b0e66392c58703f2385d00e
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-11-03 20:44:26 +0200

    Define search method through a variable

--- apps/ITC/verification/test.sh
+++ apps/ITC/verification/test.sh
@@ -1,13 +1,18 @@
 #! /bin/sh
 set -ev
+# Set default values, if variables are unset
+SEARCH_METHOD=${SEARCH_METHOD-"DBS"}
+DATASET=${DATASET-"comp01.ctt"}
 # Calculate the available time for searching a solution
 cd datasets
 AVAILABLE_SECONDS=$(yes | ./benchmark_my_linux_machine | \
                     grep "seconds" | grep -o "[[:digit:]]*")
 cd -
 # Set the available time
-sed -i "s/^\(timeLimit\) .*/\1 $AVAILABLE_SECONDS/" optionsFile && \
+sed -i "s/^\(timeLimit\) .*/\1 $AVAILABLE_SECONDS/" optionsFile
 sed -i "s/^\(timeLimitDirectMethodRound\) .*/\1 $AVAILABLE_SECONDS/" optionsFile
+# Set the search method
+sed -i "s/^\(searchMethod\) .*/\1 $SEARCH_METHOD/" optionsFile
 # Execute the curriculum based course timetabling solver
 ./itc_solver datasets/$DATASET solution.txt -options optionsFile
 # Compile the official validator
