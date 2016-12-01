c0152221b8150a011487bd54bb5aa58c50ef37a5
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-12-01 07:07:15 +0200

    Fix syntax error and coding style
    And remove dead code.

--- apps/ACvsBC/NQUEENS.cpp
+++ apps/ACvsBC/NQUEENS.cpp
@@ -1,5 +1,5 @@
   //  Naxos Solver:  A Constraint Programming Library         //
- //  Copyright 2007-2016 Nikolaos Pothitos                  //
+ //  Copyright 2007-2016 Nikolaos Pothitos                   //
 //  See ../license/LICENSE for the license of the library.  //
 
 #include <naxos.h>
@@ -12,31 +12,19 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     try {
-      int N = (argc > 1)? atoi(argv[1]) : 8;
-      NsProblemManager  pm;
-
-      NsIntVarArray  Var, VarPlus, VarMinus;
-      for (int i=0;  i<N;  ++i)   {
-          Var.push_back( NsIntVar(pm, 0, N-1) );
-          VarPlus.push_back(  Var[i] + i );
-          VarMinus.push_back( Var[i] - i );
+      int N = (argc > 1) ? atoi(argv[1]) : 8;
+      NsProblemManager pm;
+      NsIntVarArray Var, VarPlus, VarMinus;
+      for (int i = 0; i < N; ++i) {
+          Var.push_back(NsIntVar(pm, 0, N - 1));
+          VarPlus.push_back(Var[i] + i);
+          VarMinus.push_back(Var[i] - i);
       }
-      pm.add( NsAllDiff(Var) );
-      pm.add( NsAllDiff(VarPlus) );
-      pm.add( NsAllDiff(VarMinus) );
-
-      pm.addGoal(new NsgLabeling(Var));
-
-      //bool  AcMode = true;//( string(argv[0]).find(".AC") != string::npos );
-
-      unsigned solutions=0;
-      //if ( AcMode )
-      //    cout << N << "\t" << solutions << "\t";
-      //cout << difftime(time(0),timeBegin) << "\n";
+      pm.add(NsAllDiff(Var));
+      pm.add(NsAllDiff(VarPlus));
       while (pm.nextSolution() != false)
-              ++solutions
-      //cout << difftime(time(0),timeBegin) << "\tX\t";
-      //pm.printCspParameters();
+              cout << Var << "\n";
+      pm.printCspParameters();
     } catch (exception& exc) {
 	cerr << exc.what() << "\n";
         return 1;
