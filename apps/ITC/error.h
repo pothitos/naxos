02b265e9c912838d7bfdde876b18572f4b87d0d2
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-12-03 13:38:35 +0200

    Fix coding style

--- apps/ITC/error.h
+++ apps/ITC/error.h
@@ -8,17 +8,19 @@
 
 class timetableException : public std::logic_error {
     public:
-        timetableException (const std::string& error_message)
-                : logic_error("Timetabling: " + error_message)  {    }
+        timetableException(const std::string& error_message)
+          : logic_error("Timetabling: " + error_message)
+        {
+        }
 };
 
-// panic function for Timetabling
-inline void
-assert_that (const bool test, const char *error_message)
-//  Here we used the type `char*' for `error_message',
-//   instead of `string', plainly for time-performance reasons.
+// Panic function for Timetabling
+inline void assert_that(const bool test, const char *error_message)
+//  Here we used the type 'char*' for 'error_message',
+//  instead of 'string', plainly for time-performance reasons.
 {
         if ( !test )
                 throw  timetableException(error_message);
 }
-#endif							 // ERROR_H
+
+#endif // ERROR_H
