80f7df05178b634d643f2cc0d2e98030250070a3
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-12-17 12:36:41 +0200

    Compare AC vs. BC efficiency in Magic Square CSP

--- apps/ACvsBC/experiments.sh
+++ apps/ACvsBC/experiments.sh
@@ -3,12 +3,17 @@ set -e
 
 echo "CSP\tParam\tn\td\te\tAC_Time\tBC_Time"
 echo
-PARAM=10
-while [ $PARAM -le 15 ]
+for PARAM in $(seq 10 15)
 do
     time -o AC_Time.txt -f "%e" ./nqueens.AC $PARAM > /dev/null
     time -o BC_Time.txt -f "%e" ./nqueens $PARAM | tail -1 > n_d_e.txt
     echo "NQueens\t$PARAM\t$(cat n_d_e.txt)\t$(cat AC_Time.txt)\t$(cat BC_Time.txt)"
-    PARAM=$(($PARAM + 1))
+done
+echo
+for PARAM in $(seq 7 9)
+do
+    time -o AC_Time.txt -f "%e" ./magic_square.AC $PARAM > /dev/null
+    time -o BC_Time.txt -f "%e" ./magic_square $PARAM | tail -1 > n_d_e.txt
+    echo "MagicSq\t$PARAM\t$(cat n_d_e.txt)\t$(cat AC_Time.txt)\t$(cat BC_Time.txt)"
 done
 rm n_d_e.txt AC_Time.txt BC_Time.txt
