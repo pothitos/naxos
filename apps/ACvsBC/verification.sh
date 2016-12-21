9ce08472eaf981eb8533d58e2e29a64ddcbd0063
Nikolaos Pothitos <pothitos@di.uoa.gr>
2016-12-21 15:39:25 +0200

    Escape slash character in sed

--- apps/ACvsBC/verification.sh
+++ apps/ACvsBC/verification.sh
@@ -3,7 +3,7 @@ set -ev
 # Set parameters for small CSP instances
 sed -i.bak 's/^\(NQUEENS_RANGE\)=.*/\1=$(seq 10 11)/' experiments.sh
 sed -i 's/^\(MAGIC_SQUARE_RANGE\)=.*/\1=$(seq 5 6)/' experiments.sh
-sed -i 's/^\(CREW_SCHEDULING_RANGE\)=.*/\1=OR-Library/NW41.txt' experiments.sh
+sed -i 's/^\(CREW_SCHEDULING_RANGE\)=.*/\1=OR-Library\/NW41.txt/' experiments.sh
 sed -i 's/^\(TSP_RANGE\)=.*/\1=$(seq 8 9)/' experiments.sh
 # Execute the measurements
 ./experiments.sh | tee ACvsBC.dat
