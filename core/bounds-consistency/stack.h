e13c99581a859fa2bc67eb0f198f71b9569f29f6
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-01-13 15:51:35 +0200

    Break up long strings

--- core/bounds-consistency/stack.h
+++ core/bounds-consistency/stack.h
@@ -132,15 +132,15 @@ class NsStack {
 
                 TemplType& operator * (void) const
                 {
-                        assert_Ns(currNode != 0,
-                                  "NsStack::iterator::*: Bad request '*(something.end())'");
+                        assert_Ns(currNode != 0, "NsStack::iterator::*: Bad "
+                                  "request '*(something.end())'");
                         return currNode->theData;
                 }
 
                 TemplType *operator -> (void) const
                 {
-                        assert_Ns(currNode != 0,
-                                  "NsStack::iterator::->: Bad request '*(something.end())'");
+                        assert_Ns(currNode != 0, "NsStack::iterator::->: Bad "
+                                  "request '*(something.end())'");
                         return &currNode->theData;
                 }
 
@@ -152,8 +152,8 @@ class NsStack {
 
                 iterator& operator ++ (void)
                 {
-                        assert_Ns(currNode != 0,
-                                  "NsStack::iterator::++: Bad request '++(something.end())'");
+                        assert_Ns(currNode != 0, "NsStack::iterator::++: Bad "
+                                  "request '++(something.end())'");
                         currNode = currNode->next;
                         return *this;
                 }
@@ -200,15 +200,15 @@ class NsStack {
 
                 const TemplType& operator * (void) const
                 {
-                        assert_Ns(currNode != 0,
-                                  "NsStack::const_iterator::*: Bad request '*(something.end())'");
+                        assert_Ns(currNode != 0, "NsStack::const_iterator::*: "
+                                  "Bad request '*(something.end())'");
                         return currNode->theData;
                 }
 
                 const TemplType *operator -> (void) const
                 {
-                        assert_Ns(currNode != 0,
-                                  "NsStack::const_iterator::->: Bad request '*(something.end())'");
+                        assert_Ns(currNode != 0, "NsStack::const_iterator::->: "
+                                  "Bad request '*(something.end())'");
                         return &currNode->theData;
                 }
 
@@ -220,8 +220,8 @@ class NsStack {
 
                 const_iterator& operator ++ (void)
                 {
-                        assert_Ns(currNode != 0,
-                                  "NsStack::const_iterator::++: Bad request '++(something.end())'");
+                        assert_Ns(currNode != 0, "NsStack::const_iterator::++: "
+                                  "Bad request '++(something.end())'");
                         currNode = currNode->next;
                         return *this;
                 }
