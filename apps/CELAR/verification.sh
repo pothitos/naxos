cf022d45f0e23ffaaef4711fa98167f1a11a19b3
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-01-28 00:26:11 +0200

    Add memory test

--- apps/CELAR/verification.sh
+++ apps/CELAR/verification.sh
@@ -2,7 +2,13 @@
 set -ev
 # Set default values, if variables are unset
 DATASET=${DATASET-"SCEN06"}
+# If Valgrind is installed, do memory check too
+if command -v valgrind
+then
+    MEM_CHECK="valgrind -q --leak-check=full --error-exitcode=1"
+fi
 # The second field of the output should be the objective value
-./celar_solver $DATASET | grep "^[^	]*	[[:digit:]]\+	"
+$MEM_CHECK ./celar_solver $DATASET > solution.txt
+grep "^[^	]*	[[:digit:]]\+	" solution.txt
 # Clean up
-rm $DATASET/sol.txt
+rm $DATASET/sol.txt solution.txt
