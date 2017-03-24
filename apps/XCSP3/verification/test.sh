b84c77b377cbc37575e8fa421aae3ab07f3d7f3e
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-03-24 02:32:41 +0200

    Only warn for memory issues
    Disable memory errors, due to reported leaks in system
    libraries. We cannot fix them directly from this project.

--- apps/XCSP3/verification/test.sh
+++ apps/XCSP3/verification/test.sh
@@ -2,7 +2,7 @@
 set -ev
 
 # Check for memory leaks
-MEM_CHECK="valgrind -q --leak-check=full --error-exitcode=1"
+MEM_CHECK="valgrind -q --leak-check=full"
 $MEM_CHECK ./naxos-xcsp3 parser/src/XCSP3-CPP-Parser/instances/tsp-25-843.xml \
     > /dev/null
 
