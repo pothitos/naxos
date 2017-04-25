256b9fabb38ed8076c2df3cee3335358ff5b13a4
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-04-25 08:31:40 +0300

    Add CSP instance with variables and arrays only

---
+++ apps/XCSP3/verification/Arrays.sol
@@ -0,0 +1,5 @@
+s SATISFIABLE
+v <instantiation>
+v   <list> X ArrayA[0] ArrayA[1] ArrayA[2] ArrayB[0][0] ArrayB[0][1] ArrayB[1][0] ArrayB[1][1] ArrayC[0][0][0] ArrayC[0][0][1] ArrayC[0][1][0] ArrayC[0][1][1] ArrayC[1][0][0] ArrayC[1][0][1] ArrayC[1][1][0] ArrayC[1][1][1] X1 </list>
+v   <values> 0 1 1 1 2 2 -2 -2 -3 -3 -3 -3 -3 -3 -3 -3 0 </values>
+v </instantiation>
