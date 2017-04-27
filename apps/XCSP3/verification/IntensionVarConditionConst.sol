1a50997ffe52f903f5ca9d6831b8d635ad1c4f0f
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-04-27 23:52:38 +0300

    Add intension constraint between variable and constant

---
+++ apps/XCSP3/verification/IntensionVarConditionConst.sol
@@ -0,0 +1,5 @@
+s SATISFIABLE
+v <instantiation>
+v   <list> X[0][0] X[0][1] X[0][2] X[0][3] X[0][4] X[0][5] X[1][0] X[1][1] X[1][2] X[1][3] X[1][4] X[1][5] </list>
+v   <values> 3 2 1 1 4 5 4 2 3 4 1 1 </values>
+v </instantiation>
