515069b95cdf0e028b2065388bfbfee53bfdb132
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-05-17 22:59:52 +0300

    Search for files from the top directory

--- apps/XCSP3/verification/test.sh
+++ apps/XCSP3/verification/test.sh
@@ -18,11 +18,11 @@ test $SLOC -lt 8000
 # Check coding style in TraviS CI
 if [ "$CONTINUOUS_INTEGRATION" = "true" ]
 then
+    cd -
     # Fix coding style of all source files
-    find ../.. \( -iname "*.h" -or -iname "*.cpp" \) \
+    find \( -iname "*.h" -or -iname "*.cpp" \) \
         -exec clang-format-5.0 -i {} +
     # List the files that may need reformatting
-    cd -
     git ls-files -m
     cd -
 fi
