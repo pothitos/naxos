962cde4d4a17601c32b05aec365637d466322b77
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-05-30 15:53:25 +0300

    Create file for unneeded mini-solver constraints

---
+++ core/non_mini_solver_constraints.cpp
@@ -0,0 +1,9 @@
+/// @file
+/// Unnecessary constraints for the XCSP3 Mini-Solver competition
+///
+/// Part of https://github.com/pothitos/naxos
+
+#include "naxos.h"
+
+using namespace naxos;
+using namespace std;
