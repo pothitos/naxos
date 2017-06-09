910c5c042e8002bd45a5b7d71df893aa596db2dd
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-06-09 14:57:29 +0300

    Add missing variable's name

--- apps/XCSP3/verification/Extension.sol
+++ apps/XCSP3/verification/Extension.sol
@@ -1,5 +1,5 @@
 s SATISFIABLE
 v <instantiation>
-v   <list> A[0] A[1] B[0] B[1] B[2] C[0] C[1] C[2] X </list>
+v   <list> A[0] A[1] B[0] B[1] B[2] C[0] C[1] C[2] X Y </list>
 v   <values> 3 2 1 1 2 1 2 1 3 3 </values>
 v </instantiation>
