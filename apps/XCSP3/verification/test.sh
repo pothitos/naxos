91e6b9a6c78990638a9d3ddbc000be935ddab8c8
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-06-24 12:58:06 +0300

    Validate XCSP3 solutions

--- apps/XCSP3/verification/test.sh
+++ apps/XCSP3/verification/test.sh
@@ -31,19 +31,36 @@ fi
 # Memory check tool
 MEM_CHECK="valgrind -q"
 
+# Solution validation tool
+VALIDATOR="java -classpath checker/src/XCSP3-Java-Tools/src/main/java org.xcsp.checker.SolutionChecker -cm"
+
+# Temporary file
+SOLUTION="/tmp/instance.sol"
+
 # Default Traveling Salesman Problem instance
-$MEM_CHECK ./naxos-xcsp3 parser/src/XCSP3-CPP-Parser/instances/tsp-25-843.xml
+INSTANCE="parser/src/XCSP3-CPP-Parser/instances/tsp-25-843.xml"
+$MEM_CHECK ./naxos-xcsp3 $INSTANCE > $SOLUTION
+$VALIDATOR $INSTANCE $SOLUTION
 
 # Default Constraint Optimisation (COP) instance
-$MEM_CHECK ./naxos-xcsp3 parser/src/XCSP3-CPP-Parser/instances/obj.xml
+INSTANCE="parser/src/XCSP3-CPP-Parser/instances/obj.xml"
+$MEM_CHECK ./naxos-xcsp3 $INSTANCE > $SOLUTION
+$VALIDATOR $INSTANCE $SOLUTION
 
 # Limit the available time to 10s for searching a solution
+INSTANCE="verification/without_solutions/AllConstraints.xml"
 timeout --preserve-status --kill-after=1s 10s \
-    $MEM_CHECK ./naxos-xcsp3 verification/without_solutions/AllConstraints.xml
+    $MEM_CHECK ./naxos-xcsp3 $INSTANCE > $SOLUTION
+$VALIDATOR $INSTANCE $SOLUTION
 
 # Reduce the available time to 5s, while not testing memory
+INSTANCE="verification/without_solutions/AllConstraintsFormatted.xml"
 timeout --preserve-status --kill-after=1s 5s \
-    ./naxos-xcsp3 verification/without_solutions/AllConstraintsFormatted.xml
+    ./naxos-xcsp3 $INSTANCE > $SOLUTION
+$VALIDATOR $INSTANCE $SOLUTION
+
+# Clean up
+rm $SOLUTION
 
 # For each Mini-solver Competition's requirement, solve a CSP
 for INSTANCE in verification/*.xml
@@ -52,4 +69,5 @@ do
     SOLUTION="verification/$(basename $INSTANCE .xml).sol"
     # Compare the stored solution with the solver's one
     ./naxos-xcsp3 $INSTANCE | cmp $SOLUTION
+    $VALIDATOR $INSTANCE $SOLUTION
 done
