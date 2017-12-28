f8cfaa060d2ef83d95595b72a5882dfc5f7eef71
Nikolaos Pothitos <pothitos@di.uoa.gr>
2017-12-28 10:03:47 +0200

    Remove mini-solver constraints
    According to http://xcsp.org/call2018.pdf "A mini-solver is
    a solver whose code must be open-source (and expected to be
    rather compact, although we do not impose anymore specific
    restrictions)."

--- apps/XCSP3/verification/test.sh
+++ apps/XCSP3/verification/test.sh
@@ -48,21 +48,6 @@ validate() {
 
 set -ev
 
-SOLVER_FILES=$(ls ../../core/*.h ../../core/*.cpp | \
-               grep -v "non_mini_solver\|naxos.h")
-# Ensure that Naxos Solver can be considered as a "mini-solver."
-# According to the First International XCSP3 Competition: "A
-# mini-solver is a solver whose code must be composed of
-# less than 8,000 lines of at most 160 characters (while
-# discarding code for parsing XCSP3, comments and code of
-# standard libraries)."
-SLOC=$(cat $SOLVER_FILES | grep -v "^$" | grep -v "^ *//" | wc -l)
-echo "$SLOC pure source lines of code"
-test $SLOC -lt 8000
-
-# Ensure that the maximum line width limit isn't exceeded
-(! grep ".\{161\}" $SOLVER_FILES)
-
 # Check coding style in TraviS CI
 if [ "$CONTINUOUS_INTEGRATION" = "true" ]
 then
