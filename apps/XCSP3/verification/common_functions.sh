7b5923353b0eeb10289b58d455fb90a60610b2fa
Nikolaos Pothitos <pothitos@di.uoa.gr>
2018-02-27 21:05:20 +0200

    Isolate common testing functions

---
+++ apps/XCSP3/verification/common_functions.sh
@@ -0,0 +1,61 @@
+#! /bin/sh
+
+validate() {
+    # Solution validation tool
+    VALIDATOR="java -classpath
+               verification/XCSP3-Java-Tools/build/libs/xcsp3-tools-*.jar
+               org.xcsp.checker.SolutionChecker -cm"
+
+    COST=$(grep "^o -\?[[:digit:]]\+$" $SOLUTION | tail -1 |
+           grep -o -- "-\?[[:digit:]]\+" || true)
+    VALIDATION=$($VALIDATOR $INSTANCE $SOLUTION 2>&1)
+    ! VALIDATION=$(echo "$VALIDATION" |
+                   grep -v "^Picked up _JAVA_OPTIONS: \|^ROOT\|^$")
+    if echo "$VALIDATION" | grep -q \
+                            "^ERROR: the instantiation cannot be checked "
+    then
+        echo "$INSTANCE: Cannot check the instantiation"
+    elif [ "$VALIDATION" = "" ]
+    then
+        echo
+        echo "$INSTANCE:"
+        cat $SOLUTION
+    elif [ "$VALIDATION" != "OK	$COST" ]
+    then
+        echo
+        echo "Wrong solution for $INSTANCE:"
+        cat $SOLUTION
+        echo "$VALIDATION"
+        exit 1
+    fi
+}
+
+validate_exit_status() {
+    set -e
+    STATUS="$1"
+    if [ -z "$STATUS" ]
+    then
+        echo "validate_exit_status: Missing status argument!" 1>&2
+        exit 1
+    fi
+    if [ $STATUS -eq 137 ]
+    then
+        #     Killed
+        echo "       $INSTANCE before normal termination"
+        if [ -s $SOLUTION ]
+        then
+            validate
+        fi
+    else
+        if [ $STATUS -ne 0 ]
+        then
+            echo "$INSTANCE: Unexpected exit status $STATUS"
+            if [ -s $SOLUTION ]
+            then
+                cat $SOLUTION
+            fi
+            exit $STATUS
+        fi
+        validate
+    fi
+}
