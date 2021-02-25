3b94cff34250d283177239117128d132b2cb85d9
Hector Tavoularis <43973352+hectortav@users.noreply.github.com>
2021-02-25 19:52:20 +0200

    Migrate from travis CI and Appveyor to github actions

--- apps/verification/test.sh
+++ apps/verification/test.sh
@@ -5,15 +5,15 @@ $MEM_CHECK ./nqueens 3 > solutions.txt
 sort solutions.txt | cmp /dev/null
 # Validate 4 Queens solutions
 $MEM_CHECK ./nqueens 4 > solutions.txt
-sort solutions.txt | cmp verification/nqueens_4_sorted_solutions.txt
+bash -c 'cmp <(sort solutions.txt) <(sort verification/nqueens_4_sorted_solutions.txt)'
 # Validate 8 Queens solutions
 $MEM_CHECK ./nqueens 8 > solutions.txt
-sort solutions.txt | cmp verification/nqueens_8_sorted_solutions.txt
+bash -c 'cmp <(sort solutions.txt) <(sort verification/nqueens_8_sorted_solutions.txt)'
 # Validate 12 Queens solutions
 ./nqueens 12 > solutions.txt
-sort solutions.txt | cmp verification/nqueens_12_sorted_solutions.txt
+bash -c 'cmp <(sort solutions.txt) <(sort verification/nqueens_12_sorted_solutions.txt)'
 # Validate SEND + MORE = MONEY solution
 $MEM_CHECK ./send_more_money > solutions.txt
-cmp solutions.txt verification/send_more_money_solution.txt
+bash -c 'cmp <(sort solutions.txt) <(sort verification/send_more_money_solution.txt)'
 # Clean up
 rm solutions.txt
